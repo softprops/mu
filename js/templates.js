ed192276e0c37a8e64668eed00d9d9ef51deaeb0
softprops <d.tangren@gmail.com>
2010-11-02 01:25:42 -0400

    + -

--- js/templates.js
+++ js/templates.js
@@ -1,15 +1,18 @@
 /* Templates */
 var T = (function() {
   var Event = '<a href="{0}" target="_blank"> \
-       <div class="iwrap"><img class="g" src="{1}"/><div class="imeta"><span class="rsvpcnt">+{2}</span></div></div> \
+       <div class="iwrap"> \
+          <img class="g" src="{1}"/> \
+          <div class="imeta"><span class="rsvpcnt">+{2}<br/>- {3}</span></div> \
+       </div> \
        <div class="emeta"> \
-         <strong>{3}</strong> <span class="ename">{4}</span> \
-         <div><span class="where">@<a target="_blank" href="http://maps.google.com/maps?q={5}">{5}</a></span> \
+         <strong>{4}</strong> <span class="ename">{5}</span> \
+         <div><span class="where">@<a target="_blank" href="http://maps.google.com/maps?q={6}">{6}</a></span> \
          <span class="when">on {6}</span></div> \
        </div> \
       </a>';
   var EventWithoutLocation = '<a href="{0}" target="_blank"> \
-       <div class="iwrap"><img class="g" src="{1}"/><div class="imeta"><span class="rsvpcnt">+{2}</span></div></div> \
+       <div class="iwrap"><img class="g" src="{1}"/><div class="imeta"><span class="rsvpcnt">+{2}<br/>- {3}</span></div></div> \
        <div class="emeta"> \
          <strong>{3}</strong> <span class="ename">{4}</span>\
          <div><span class="when">on {5}</span></div> \
@@ -24,9 +27,9 @@ var T = (function() {
     },
     event: function(e) {
       if(e.venue_address1.toString().length === 0) {
-        return EventWithoutLocation.format(e.event_url, e.photo_url, e.rsvpcount, e.group_name, e.name, e.time); 
+        return EventWithoutLocation.format(e.event_url, e.photo_url, e.rsvpcount, e.no_rsvpcount, e.group_name, e.name, e.time); 
       } else { 
-        return Event.format(e.event_url, e.photo_url, e.rsvpcount, e.group_name, e.name, e.venue_address1, e.time);
+          return Event.format(e.event_url, e.photo_url, e.rsvpcount, e.no_rsvpcount, e.group_name, e.name, e.venue_address1, e.time);
       }
     },
     user: function(u) {
