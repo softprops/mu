07a05dcf6c3c482905bafda99d857bf79b8e44ea
softprops <d.tangren@gmail.com>
2010-11-01 23:40:06 -0400

    target _blank in links

--- js/templates.js
+++ js/templates.js
@@ -1,14 +1,14 @@
 /* Templates */
 var T = (function() {
-  var Event = '<a href="{0}"> \
+  var Event = '<a href="{0}" target="_blank"> \
        <div class="iwrap"><img class="g" src="{1}"/><div class="imeta"><span class="rsvpcnt">+{2}</span></div></div> \
        <div class="emeta"> \
          <strong>{3}</strong> <span class="ename">{4}</span> \
-         <div><span class="where">@<a href="http://maps.google.com/maps?q={5}">{5}</a></span> \
+         <div><span class="where">@<a target="_blank" href="http://maps.google.com/maps?q={5}">{5}</a></span> \
          <span class="when">on {6}</span></div> \
        </div> \
       </a>';
-  var EventWithoutLocation = '<a href="{0}"> \
+  var EventWithoutLocation = '<a href="{0}" target="_blank"> \
        <div class="iwrap"><img class="g" src="{1}"/><div class="imeta"><span class="rsvpcnt">+{2}</span></div></div> \
        <div class="emeta"> \
          <strong>{3}</strong> <span class="ename">{4}</span>\
@@ -16,7 +16,7 @@ var T = (function() {
        </div> \
       </a>'; 
 
-  var User = 'Hi <a class="uname" href="{0}">{1}</a> | <a class="out" href="#">Disconnect</a>';
+  var User = 'Hi <a class="uname" href="{0}" target="_blank">{1}</a> | <a class="out" href="#">Disconnect</a>';
 
   return {
     group: function(g) {
