79bccaaee4f5106a0f89a2b918d8f7227b224587
softprops <d.tangren@gmail.com>
2010-11-02 09:31:54 -0400

    jquery is ok

--- js/mu.js
+++ js/mu.js
@@ -1,21 +1,6 @@
 var M = (function(conf) {
   var api = "https://api.meetup.com/",
       key = conf.key;
-      /*,get = function(path, params, fn) {
-        var req = new XMLHttpRequest();
-        var url = function() {
-          var r = [], append = function(k, v) {
-            r[r.length] = encodeURIComponent(k) + "=" + encodeURIComponent(v);
-          };
-          for(k in params) append(k, params[k])
-          return api + path + (r.join("&").replace(/%20/g, "+"));
-        };
-        req.open(url(), "GET", true);
-        req.onload = function() {
-          fn(JSON.parse(req.responseText));
-        }
-        req.send(null);
-      };*/
   
   var sign = function(args) {
      args["key"] = key;
